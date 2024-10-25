5c64614cf3922d0c175961e0dd670abff993743b
Mateusz Jenek <mateusz.jenek@gmail.com>
2024-10-25 14:00:52 +0200

    fix: replace secret store on id change

--- internal/provider/resource_secret_store.go
+++ internal/provider/resource_secret_store.go
@@ -9,6 +9,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/humanitec/humanitec-go-autogen"
 	"github.com/humanitec/humanitec-go-autogen/client"
@@ -92,6 +94,9 @@ func (*SecretStore) Schema(ctx context.Context, req resource.SchemaRequest, resp
 			"id": schema.StringAttribute{
 				MarkdownDescription: "The ID of the Secret Store.",
 				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"primary": schema.BoolAttribute{
 				MarkdownDescription: "Whether the Secret Store is the Primary one for the organization.",
